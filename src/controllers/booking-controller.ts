7dc70144b64a8aed8e2a5a8b12f4b732d8b8f1ea
Giovanna <giovannad1clonius@gmail.com>
2023-07-21 15:59:29 -0300

    fix

--- src/controllers/booking-controller.ts
+++ src/controllers/booking-controller.ts
@@ -22,7 +22,7 @@ export async function postBookings(req: AuthenticatedRequest, res: Response){
 
     try{
         const booking = await bookingService.postBookings(userId, roomId);
-        return res.status(httpStatus.OK).send({booking});
+        return res.status(httpStatus.OK).send({bookingId: booking});
     } catch (err) {
         if(err.name === "NotFoundError"){
             return res.sendStatus(httpStatus.NOT_FOUND);
@@ -40,7 +40,7 @@ export async function updateBookings(req: AuthenticatedRequest, res: Response){
 
     try {
         const booking = await bookingService.updateBookings(userId, roomId, Number(bookingId));
-        return res.status(httpStatus.OK).send({booking});
+        return res.status(httpStatus.OK).send({bookingId: booking});
     } catch (err) {
         if(err.name === "NotFoundError"){
             return res.sendStatus(httpStatus.NOT_FOUND);
